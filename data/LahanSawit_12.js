16de34fb8a1454687f03d63b5176944c080cf5ab
Fadila Suhaila <113318558+FADILASUHAILA@users.noreply.github.com>
2025-01-03 04:31:03 +0700

    Add files via upload

---
+++ data/LahanSawit_12.js
@@ -0,0 +1,1 @@
+var json_LahanSawit_12 = {"type":"FeatureCollection","name":"LahanSawit_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/Warkop_19.js
@@ -0,0 +1,1 @@
+var json_Warkop_19 = {"type":"FeatureCollection","name":"Warkop_19","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
